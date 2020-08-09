f2743f1cafbc3a88c17dd724043d0eb5cf40dc1b
popcorn <popcorn9499@gmail.com>
2020-08-09 15:50:45 -0500

    honestly basic setup. nothing to see here

---
+++ ProcessAudiobooks-UI/ConsoleWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ProcessAudiobooks_UI
+{
+    class ConsoleWindow
+    {
+        public enum DEBUG_LEVELS
+        {
+            INFO = 10,
+            WARNING = 20,
+            ERROR = 30,
+            CRITICAL = 40,
+            DEBUG = 50,
+        }
+
+        private int debugLevel = (int)DEBUG_LEVELS.DEBUG;
+
+        private static string PREFIX = "[ProcessAudioBooks-UI]: ";
+
+        public ConsoleWindow()
+        {
+            AllocConsole();
+            this.WriteDebug(" Started");
+        }
+
+
+        private void _Write(String msg)
+        {
+            Console.WriteLine(ConsoleWindow.PREFIX + msg);
+        }
+
+        public void WriteInfo(string msg)
+        {
+            if (debugLevel >= (int)ConsoleWindow.DEBUG_LEVELS.INFO)
+            {
+                this._Write("[INFO] " + msg);
+            }
+        }
+
+        public void WriteWarning(string msg)
+        {
+            if (debugLevel >= (int)ConsoleWindow.DEBUG_LEVELS.WARNING)
+            {
+                this._Write("[WARNING] " + msg);
+            }
+        }
+
+        public void WriteError(string msg)
+        {
+            if (debugLevel >= (int)ConsoleWindow.DEBUG_LEVELS.ERROR)
+            {
+                this._Write("[ERROR] " + msg);
+            }
+        }
+
+        public void WriteCritical(string msg)
+        {
+            if (debugLevel >= (int)ConsoleWindow.DEBUG_LEVELS.CRITICAL)
+            {
+                this._Write("[CRITICAL] " + msg);
+            }
+        }
+
+        public void WriteDebug(string msg)
+        {
+            if (debugLevel >= (int)ConsoleWindow.DEBUG_LEVELS.DEBUG)
+            {
+                this._Write("[DEBUG] " + msg);
+            }
+        }
+
+        //these 2 imports allow us to create a console window in a wpf application.
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+    }
+
+
+}
---
+++ ProcessAudiobooks-UI/ssh.cs
@@ -0,0 +1,55 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessAudiobooks_UI
+{
+    class ssh
+    {
+         public ssh() {
+
+            //Console.WriteLine("Starting Audiobook Processor");
+            var tsk = TestMethod();
+            for (int i = 0; i< 5; i++)
+            {
+                Console.WriteLine("TEST123");
+                Thread.Sleep(1000);
+            }
+            tsk.Wait();
+
+        }
+
+        public static async Task TestMethod()
+        {
+            using (var client = new SshClient("ip", "username", "test"))
+            {
+                client.Connect();
+                var cmd = client.CreateCommand("for i in {1..5}; do echo 'hi' ; sleep 1 ; done");
+                var result = cmd.BeginExecute();
+               // Console.WriteLine(cmd.Result);
+
+                using (var reader =
+                   new StreamReader(cmd.OutputStream, Encoding.UTF8, true, -1, true))
+                {
+                    while (!result.IsCompleted || !reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            Console.WriteLine(line);
+                            await Task.Delay(50);
+                        }
+                    }
+                }
+
+                cmd.EndExecute(result);
+
+
+            }
+        }
+    }
+}
