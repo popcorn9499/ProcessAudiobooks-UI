d7479fd77081dab0020da8a6161659e2a655e47b
popcorn <popcorn9499@gmail.com>
2020-08-11 14:41:01 -0500

    Audiobook status enum

---
+++ ProcessAudiobooks-UI/DataObjects/AudiobookProcessingStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessAudiobooks_UI.DataObjects
+{
+    public enum AudiobookProcessingStatus
+    { //An easy way to keep track of my audiobook status
+        Ready = 0,
+        Processing = 1,
+        Completed = 2
+    }
+}
