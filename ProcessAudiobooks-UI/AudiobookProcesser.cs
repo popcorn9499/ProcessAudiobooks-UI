a85f7c557887b79fea82f0d4ef4dcc7821a3c683
popcorn <popcorn9499@gmail.com>
2020-08-12 01:50:24 -0500

    gave it the function to copy to a specific folder
    this will grant us the functionality to have a knowable path between the server and client

--- ProcessAudiobooks-UI/AudiobookProcesser.cs
+++ ProcessAudiobooks-UI/AudiobookProcesser.cs
@@ -1,7 +1,10 @@
 ﻿using ProcessAudiobooks_UI.CustomControls;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ProcessAudiobooks_UI
@@ -28,7 +31,54 @@ namespace ProcessAudiobooks_UI
             if (this.processing == Processing.Stopped) //if not started start.
             {
                 this.processing = Processing.Started;
+                foreach (DataObjects.Audiobook book in eLvAudiobook.Items)
+                {
+                    if (book.Status == DataObjects.AudiobookProcessingStatus.Ready)
+                    {
+                        string destFolder = tbLocalPath.Text + "\\" + book.Name;
+                        Directory.CreateDirectory(destFolder);
+                        string[] fileList = book.FileList.ToArray();
+                        foreach (String file in fileList)
+                        {
+                            // get the file attributes for file or directory
+                            FileAttributes attr = File.GetAttributes(file);
+                            string[] fileArray = file.Split("\\");
+                            string fileName = fileArray[fileArray.Length - 1];
 
+                            //detect whether its a directory or file
+                            if ((attr & FileAttributes.Directory) == FileAttributes.Directory) // if its a directory copy the directory and files individually
+                            {
+                                string trueDestFolder = destFolder + "\\" + fileName;
+                                Directory.CreateDirectory(trueDestFolder);
+                                //Now Create all of the directories
+                                foreach (string dirPath in Directory.GetDirectories(file, "*",
+                                    SearchOption.AllDirectories))
+                                    Directory.CreateDirectory(dirPath.Replace(file, trueDestFolder));
+
+                                //Copy all the files & Replaces any files with the same name
+                                foreach (string newPath in Directory.GetFiles(file, "*.*",
+                                    SearchOption.AllDirectories))
+                                    File.Copy(newPath, newPath.Replace(file, trueDestFolder), true);
+                            }
+                            else //if its a file just do a simple file copy
+                            {
+                                System.IO.File.Copy(file, file.Replace(file, destFolder + "\\" + fileName), true);
+                            }
+
+
+                            
+
+                            /**/
+
+
+                            /*int startPos = file.CompareTo(tbLocalPath.Text);
+                            startPos = startPos + tbLocalPath.Text.Length-1;
+                            string newFile = file.Substring(startPos);
+                            newFile = tbRemotePath.Text + newFile.Replace('\\', '/');
+                            MessageBox.Show(newFile);*/
+                        }
+                    }
+                }
             }
         }
 
