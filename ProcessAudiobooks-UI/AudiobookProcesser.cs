59cef07d06672296b64c5a185e1d47a91ab3d48d
popcorn <popcorn9499@gmail.com>
2022-12-09 17:20:31 -0600

    migrated to another function to make slightly cleaner

--- ProcessAudiobooks-UI/AudiobookProcesser.cs
+++ ProcessAudiobooks-UI/AudiobookProcesser.cs
@@ -42,81 +42,7 @@ namespace ProcessAudiobooks_UI
                 this.btnStopCreateAudiobooks.IsEnabled = true;
                 foreach (DataObjects.Audiobook book in eLvAudiobook.Items)
                 {
-                    //if the book is ready to be processed and Processing hasnt been set to stop indicating we should stop mid execution of this loop. 
-                    if (book.Status == DataObjects.AudiobookProcessingStatus.Ready && this.processing != Processing.Stopped) 
-                    {
-                        book.Status = DataObjects.AudiobookProcessingStatus.Processing; //set audiobook status
-                        this.eLvAudiobook.Items.Refresh();
-                        ConsoleWindow.WriteInfo("Starting Audiobook: " + book.Name);
-                        ConsoleWindow.WriteInfo("Copying Audiobook to processing path");
-                        string destFolder = tbLocalPath.Text + "\\" + book.outputName;
-                        Directory.CreateDirectory(destFolder);
-                        string[] fileList = book.FileList.ToArray();
-                        foreach (String file in fileList)
-                        {
-                            // get the file attributes for file or directory
-                            FileAttributes attr = File.GetAttributes(file);
-                            string[] fileArray = file.Split("\\");
-                            string fileName = fileArray[fileArray.Length - 1];
-
-                            //detect whether its a directory or file
-                            if ((attr & FileAttributes.Directory) == FileAttributes.Directory) // if its a directory copy the directory and files individually
-                            {
-                                string trueDestFolder = destFolder + "\\" + fileName;
-                                this.CopyDirectory(file, trueDestFolder);
-                            }
-                            else //if its a file just do a simple file copy
-                            {
-                                System.IO.File.Copy(file, file.Replace(file, destFolder + "\\" + fileName), true);
-                            }
-                        }
-                        //Create Command
-
-                        String command = tbCommand.Text;
-                        string cmdOutputPath = "output/" + book.outputName;
-                        command = this.addVariables(command, cmdOutputPath, book);
-
-                        string commandDestFolder = tbRemotePath.Text + "/" + book.outputName;
-
-                        //Run Command
-                        ConsoleWindow.WriteInfo("Processing the audiobook");
-                        MessageBoxResult retryConnection = MessageBoxResult.No; //handle retrying to connect if for whatever reason the ssh server refuses to respond.
-                        do //loop over this code until either the user gives up or 
-                        {
-                            try
-                            {
-                                //handle running a ssh command with the sshClient
-                                await sshClient.RunCommand("cd \"" + commandDestFolder + "\" && " + command);
-                            }
-                            catch (SSHConnectionFailed)
-                            {
-                                ConsoleWindow.WriteInfo("SSH Connection Error. Retrying...");
-                                retryConnection = MessageBox.Show("Do you wish to retry connecting to the ssh server?",
-                                                                    "ProcessAudioBook Prompt", MessageBoxButton.YesNo, MessageBoxImage.Error);
-                                if (retryConnection == MessageBoxResult.No) //indicate that this loop has failed and that we should give up processing audiobooks.
-                                {
-                                    failed = true;
-                                }
-                            }
-                        } while (retryConnection == MessageBoxResult.Yes);
-
-                        if (failed)
-                        { //we must exit this existing foreach loop and proceed to reset some of the program
-                            book.Status = DataObjects.AudiobookProcessingStatus.Ready;
-                            break;
-                        }
-
-                        //copy files back
-                        string finalOutputPath = this.addVariables(book.outputPath, cmdOutputPath, book);
-                        ConsoleWindow.WriteInfo("Copying book to its output directory");
-                        this.CopyDirectory(tbLocalPath.Text + "\\" + book.outputName + "\\output\\", finalOutputPath);
-
-                        ConsoleWindow.WriteInfo("Cleaning up!");
-                        Directory.Delete(tbLocalPath.Text + "\\" + book.outputName, true);
-                        ConsoleWindow.WriteInfo("Finished Audiobook: " + book.Name);
-                        book.Status = DataObjects.AudiobookProcessingStatus.Completed; //set audiobook status
-                        this.eLvAudiobook.Items.Refresh();
-                    }
+                    await processBook(book, sshClient);
                 }
             } 
             this.processing = Processing.Stopped;
@@ -131,7 +57,81 @@ namespace ProcessAudiobooks_UI
                 MessageBox.Show("Failure occured. some audiobooks never finished being processed");
             }
         }
+        private async Task processBook(DataObjects.Audiobook book, ssh sshClient)
+        {
+       
+            //if the book is ready to be processed and Processing hasnt been set to stop indicating we should stop mid execution of this loop. 
+            if (book.Status == DataObjects.AudiobookProcessingStatus.Ready && this.processing != Processing.Stopped)
+            {
+                book.Status = DataObjects.AudiobookProcessingStatus.Processing; //set audiobook status
+                this.eLvAudiobook.Items.Refresh();
+                ConsoleWindow.WriteInfo("Starting Audiobook: " + book.Name);
+                ConsoleWindow.WriteInfo("Copying Audiobook to processing path");
+                string destFolder = tbLocalPath.Text + "\\" + book.outputName;
+                Directory.CreateDirectory(destFolder);
+                string[] fileList = book.FileList.ToArray();
+                foreach (String file in fileList)
+                {
+                    // get the file attributes for file or directory
+                    FileAttributes attr = File.GetAttributes(file);
+                    string[] fileArray = file.Split("\\");
+                    string fileName = fileArray[fileArray.Length - 1];
 
+                    //detect whether its a directory or file
+                    if ((attr & FileAttributes.Directory) == FileAttributes.Directory) // if its a directory copy the directory and files individually
+                    {
+                        string trueDestFolder = destFolder + "\\" + fileName;
+                        this.CopyDirectory(file, trueDestFolder);
+                    }
+                    else //if its a file just do a simple file copy
+                    {
+                        System.IO.File.Copy(file, file.Replace(file, destFolder + "\\" + fileName), true);
+                    }
+                }
+                //Create Command
+
+                String command = tbCommand.Text;
+                string cmdOutputPath = "output/" + book.outputName;
+                command = this.addVariables(command, cmdOutputPath, book);
+
+                string commandDestFolder = tbRemotePath.Text + "/" + book.outputName;
+
+                //Run Command
+                ConsoleWindow.WriteInfo("Processing the audiobook");
+                MessageBoxResult retryConnection = MessageBoxResult.No; //handle retrying to connect if for whatever reason the ssh server refuses to respond.
+                do //loop over this code until either the user gives up or 
+                {
+                    try
+                    {
+                        //handle running a ssh command with the sshClient
+                        await sshClient.RunCommand("cd \"" + commandDestFolder + "\" && " + command);
+                    }
+                    catch (SSHConnectionFailed)
+                    {
+                        ConsoleWindow.WriteInfo("SSH Connection Error. Retrying...");
+                        retryConnection = MessageBox.Show("Do you wish to retry connecting to the ssh server?",
+                                                            "ProcessAudioBook Prompt", MessageBoxButton.YesNo, MessageBoxImage.Error);
+                        if (retryConnection == MessageBoxResult.No) //indicate that this loop has failed and that we should give up processing audiobooks.
+                        {
+                            throw new ProcessingError();
+                        }
+                    }
+                } while (retryConnection == MessageBoxResult.Yes);
+
+
+                //copy files back
+                string finalOutputPath = this.addVariables(book.outputPath, cmdOutputPath, book);
+                ConsoleWindow.WriteInfo("Copying book to its output directory");
+                this.CopyDirectory(tbLocalPath.Text + "\\" + book.outputName + "\\output\\", finalOutputPath);
+
+                ConsoleWindow.WriteInfo("Cleaning up!");
+                Directory.Delete(tbLocalPath.Text + "\\" + book.outputName, true);
+                ConsoleWindow.WriteInfo("Finished Audiobook: " + book.Name);
+                book.Status = DataObjects.AudiobookProcessingStatus.Completed; //set audiobook status
+                this.eLvAudiobook.Items.Refresh();
+            }
+            
+        }
         private void CopyDirectory(string file, string trueDestFolder)
         {
             try
